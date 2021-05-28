bd8e733c38be4d0c663b3c504fdfadca2a1abb0e
RWagner2444 <57501007+RWagner2444@users.noreply.github.com>
2021-05-28 08:06:40 -0500

    Add files via upload

---
+++ FindMinMax.java
@@ -0,0 +1,34 @@
+
+/**
+ * This program demonstrates the use of the Integer class constants
+ * MIN_VALUE and MAX_VALUE when finding the minimum and maximum
+ * value within a numeric array.
+ *
+ * @author David J. Farber
+ * @version 06/06/17
+ */
+
+public class FindMinMax
+{
+    public static void main(String[] args)
+    {
+        int [] numList = {3, 99, -1, 5, -10};
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        System.out.println("List of numbers to find min and max values from ->");
+        for(int i: numList){
+            System.out.printf(i + " ");
+        }
+        for(int i = 0; i < numList.length; i ++)
+        {
+            if(numList[i] < min)
+                min = numList[i];
+
+            if(numList[i] > max)
+                max = numList[i];
+        }
+        System.out.println("\n\nMinimum value is " + min);
+        System.out.println("Maximum value is " + max);
+    }
+}
+
